0a7a9b34050bfede56fbb903ecfe480e591a085d
github username <github email address>
2020-03-26 10:51:26 -0400

    add

--- db/migrate/02_create_lessons.rb
+++ db/migrate/02_create_lessons.rb
@@ -1,6 +1,7 @@
 class CreateLessons < ActiveRecord::Migration[5.2]
   def change
     create_table :lessons do |t|
+      t.integer :grade_level_id
       t.string :title
       t.string :source
       t.text :description
